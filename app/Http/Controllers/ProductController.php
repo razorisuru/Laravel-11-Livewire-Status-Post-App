e32369766e8aee3f8d5f193eed5f9883a18f4496
Isuru Bandara <isurubandara318@gmail.com>
2025-01-09 22:39:24 +0530

    product-cart check

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use razorisuru\ShoppingCart\Cart;
 
 class ProductController extends Controller
 {
@@ -12,7 +13,13 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $cart = app(Cart::class);
+        $cart->add(1, 2, 100, ['color' => 'red']);
+        $cart->add(2, 2, 100, ['color' => 'green']);
+        $cart->update(2, 4);
+        // $cart->remove(1);
+        $total = $cart->total();
+        return response()->json(['cart' => $cart->getAll(), 'total' => $total]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProductController;
 
 Route::view('/', 'welcome');
 
 Route::get('chirps', [ChirpController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('chirps');
+
+    Route::get('products', [ProductController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('products');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
